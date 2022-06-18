9b602c14de20fe1b3fec4ece0953bd1f7f6495b8
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-06-17 22:32:03 -0500

    feat: add express for the server, add mongoose for the database connection

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+require('./mongo')
+
+const express = require('express')
+const app = express()
+const usersRouter = require('./routes/users')
+
+app.use(express.json())
+app.use('/api/users', usersRouter)
+
+app.get('/user', function (req, res) {
+  res.status(200).json({ name: 'john' })
+})
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hola')
+})
+
+const PORT = process.env.PORT || 3001
+const server = app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`)
+})
+
+module.exports = { app, server }
