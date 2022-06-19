84bdb7a9daca1a76b83c0b66baef9bed83cfa891
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-06-18 22:39:38 -0500

    feat: add a new controller for login

---
+++ controllers/login.js
@@ -0,0 +1,24 @@
+const User = require('../models/User')
+const { compare } = require('../utils/handlePassword')
+
+const login = async (req, res) => {
+  const { username, password } = req.body
+
+  const user = await User.findOne({ username })
+  const passwordCorrect = user === null
+    ? false
+    : await compare(password, user.passwordHash)
+
+  if (!passwordCorrect) {
+    res.status(401).json({ error: 'invalid user or password' })
+  }
+
+  res.status(200).send({
+    name: user.name,
+    username: user.username
+  })
+}
+
+module.exprts = {
+  login
+}
