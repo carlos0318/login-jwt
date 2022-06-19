87d2f44239648fa326bc915ce4a46e01111a325a
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-06-18 22:38:49 -0500

    feat: add a new router for login

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ require('./mongo')
 const express = require('express')
 const app = express()
 const usersRouter = require('./routes/users')
+const authRouter = require('./routes/auth')
 
 app.use(express.json())
 app.use('/api/users', usersRouter)
+app.use('/api', authRouter)
 
 app.get('/user', function (req, res) {
   res.status(200).json({ name: 'john' })
