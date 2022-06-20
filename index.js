7ae5feeeba18e5ab58f55c08bc348e877416dc58
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-06-19 21:54:30 -0500

    feat: add a new endpoint for /user/data that recives the token and returns the user's data

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express')
 const app = express()
 const usersRouter = require('./routes/users')
 const authRouter = require('./routes/auth')
+const { verifyToken } = require('./utils/handleJwt')
 
 app.use(express.json())
 app.use('/api/users', usersRouter)
@@ -16,6 +17,15 @@ app.get('/user', function (req, res) {
 
 app.get('/', (req, res) => {
   res.status(200).send('Hola')
+})
+
+app.get('/user/data', async function (req, res) {
+  const { authorization } = req.headers
+
+  if (!authorization) return res.status(401)
+
+  const data = await verifyToken(authorization)
+  res.status(200).send(data)
 })
 
 const PORT = process.env.PORT || 3001
